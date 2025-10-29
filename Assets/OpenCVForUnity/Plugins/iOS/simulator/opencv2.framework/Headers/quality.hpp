148d48ea321c05495bdb4bb23b1c82d5b92d0558
alidarovolj <alidarovolzhas@gmail.com>
2025-10-30 03:14:09 +0500

    Migrate OpenCV large files to Git LFS

--- Assets/OpenCVForUnity/Plugins/iOS/simulator/opencv2.framework/Headers/quality.hpp
+++ Assets/OpenCVForUnity/Plugins/iOS/simulator/opencv2.framework/Headers/quality.hpp
@@ -1,15 +1,3 @@
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html.
-
-#ifndef OPENCV_QUALITY_HPP
-#define OPENCV_QUALITY_HPP
-
-#include "quality/qualitybase.hpp"
-#include "quality/qualitymse.hpp"
-#include "quality/qualitypsnr.hpp"
-#include "quality/qualityssim.hpp"
-#include "quality/qualitygmsd.hpp"
-#include "quality/qualitybrisque.hpp"
-
-#endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab5aadbea65f20e926484b0c47c5e87c48ec019664c3cf5874abd14a88773a83
+size 469
--- Assets/OpenCVForUnity/Plugins/iOS/simulator/opencv2.framework/Headers/signal.hpp
+++ Assets/OpenCVForUnity/Plugins/iOS/simulator/opencv2.framework/Headers/signal.hpp
@@ -1,17 +1,3 @@
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html
-#ifndef OPENCV_SIGNAL_HPP
-#define OPENCV_SIGNAL_HPP
-
-/**
-* @defgroup signal Signal Processing
-* @{
-* This module includes signal processing algorithms.
-* @}
-*/
-
-#include "opencv2/core.hpp"
-#include "opencv2/signal/signal_resample.hpp"
-
-#endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:30a5e4986320a13b91b6711142cd4242bac94db4a135235a2b5ca2aef911292a
+size 436
